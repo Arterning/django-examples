e41df4996623d23d018cd8be501a09c862b8535a
Arterning <ning.huang.ridson@gmail.com>
2023-01-04 11:56:27 +0800

    type orm

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,17 +5,20 @@ import { CoffeesController } from './coffees/coffees.controller';
 import { CoffeesService } from './coffees/coffees.service';
 import { ShopCartModule } from './shop-cart/shop-cart.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Coffee } from './coffees/entities/coffee.entity';
 
 @Module({
-  imports: [ShopCartModule, TypeOrmModule.forRoot({
-    type:'postgres',
-    host:'1.12.67.61',
-    port:5432,
-    username:'strapi',
-    password:'',
-    database:'coffee',
-    autoLoadEntities:true,
-    synchronize:true,
+  imports: [ShopCartModule,
+    TypeOrmModule.forFeature([Coffee]),
+    TypeOrmModule.forRoot({
+      type:'postgres',
+      host:'1.12.67.61',
+      port:5432,
+      username:'strapi',
+      password:'',
+      database:'coffee',
+      autoLoadEntities:true,
+      synchronize:true,
   })],
   controllers: [AppController, CoffeesController],
   providers: [AppService, CoffeesService],
