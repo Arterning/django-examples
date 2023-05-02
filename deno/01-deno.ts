56e813c92686600da6bea2607e39c8a1d68af058
babyNing <ning.huang.ridson@gmail.com>
2023-05-02 13:05:20 +0800

    Extract deno code

--- deno/01-deno.ts
+++
@@ -1,9 +0,0 @@
-// deno run first_steps.ts
-
-// deno run --allow-net=deno.land first_steps.ts
-
-console.log("Welcome to Deno!");
-
-const res = await fetch("https://deno.land");
-const body = await res.text();
-console.log(body);
--- deno/02-deno-readfile.ts
+++
@@ -1,9 +0,0 @@
-/**
- * deno run --allow-read 02-deno-readfile.ts /etc/hosts
- */
-
-const filenames = Deno.args;
-for (const filename of filenames) {
-  const file = await Deno.open(filename);
-  await file.readable.pipeTo(Deno.stdout.writable, { preventClose: true });
-}
